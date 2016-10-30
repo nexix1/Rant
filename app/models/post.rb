d967f1ec7b20d6c75edab0d302755f56536365bd
Kisuk Yu <kisuk.ryu@gmail.com>
2016-10-30 04:52:39 -0400

    Added in comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy #when post gets deleted, the comment will be deleted
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
 end
